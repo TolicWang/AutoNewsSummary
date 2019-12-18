2030f7ee8be2e9e2144e8b18d1713e287a491018
tolicwang <wangchengo@126.com>
2019-12-18 16:35:18 +0800

    改变参数赋值形式

--- train.py
+++ train.py
@@ -3,17 +3,28 @@ from data.utils.load_sogou_news import SogouNews
 from data.utils.data_helper import gen_batch
 
 if __name__ == '__main__':
-    sogou_news = SogouNews(file_name='news_sohusite_xml.dat', len_words=1, freq=20)
+    len_words = 1
+    freq = 20
+    rnn_size = 128
+    rnn_layer = 2
+    truncate_times = 15
+    share_vocab = True
+    src_embed_size = 128
+    tgt_embed_size = 128
+    use_attention = False
+    batch_size = 128
+
+    sogou_news = SogouNews(file_name='news_sohusite_xml.dat', len_words=len_words, freq=freq)
     source_input, target_input, target_output, word_to_idx, idx_to_word = sogou_news.input_data()
-    model = Seq2Seq(rnn_size=128,
-                    rnn_layer=2,
-                    truncate_times=15,
-                    share_vocab=True,
+    model = Seq2Seq(rnn_size=rnn_size,
+                    rnn_layer=rnn_layer,
+                    truncate_times=truncate_times,
+                    share_vocab=share_vocab,
                     src_vocab_size=len(word_to_idx),
                     tgt_vocab_size=len(word_to_idx),
-                    src_embed_size=128,
-                    tgt_embed_size=128,
+                    src_embed_size=src_embed_size,
+                    tgt_embed_size=tgt_embed_size,
                     model_path='MODEL',
-                    batch_size=128
+                    batch_size=batch_size
                     )
-    model.train(source_input, target_input, target_output, word_to_idx, word_to_idx, gen_batch,idx_to_word)
+    model.train(source_input, target_input, target_output, word_to_idx, word_to_idx, gen_batch, idx_to_word)
